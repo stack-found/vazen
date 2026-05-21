3afb5d1bc797a41c2ba33cd4721db5821f1421ad
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-05-21 18:47:18 +0530

    feat: init config package with app and seo modules (#65)
    * feat: add postcss configuration file
    * feat: init config package

---
+++ packages/config/src/app/links.ts
@@ -0,0 +1,12 @@
+export const links = {
+  base: "https://vazen.dev",
+  cdn: "https://cdn.vazen.dev",
+  og: `https://cdn.vazen.dev/brand/opengraph-image.jpeg`,
+  logo: "https://cdn.vazen.dev/brand/logo.png",
+  github: "https://github.com/stack-found/vazen",
+  facebook: "",
+  x: "",
+  instagram: "",
+  linkedin: "",
+  bluesky: "",
+};
