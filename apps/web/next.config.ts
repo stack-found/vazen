7ea3f39fabccdb4c6a7207d936cf083183b478e3
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-05-21 23:20:19 +0530

    feat: add bundle analyzer support & update environment configuration

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,17 +1,30 @@
 import type { NextConfig } from "next";
+import bundleAnalyzer from "@next/bundle-analyzer";
 import { withSentry } from "@repo/telemetry/sentry/with-sentry";
 
 const nextConfig: NextConfig = {
-  poweredByHeader: false,
+  reactStrictMode: true,
   reactCompiler: true,
-  cacheComponents: true,
+  poweredByHeader: false,
   typedRoutes: true,
+  cacheComponents: true,
+  compress: true,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+    browserToTerminal: true,
+  },
   transpilePackages: ["@repo/ui"],
   allowedDevOrigins: ["local.web.vazen.id", "*.local.web.vazen.id"],
 };
 
+const bundleAnalyzerPlugin = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const NextApp = () => {
-  const plugins = [withSentry];
+  const plugins = [bundleAnalyzerPlugin, withSentry];
   return plugins.reduce((config, plugin) => plugin(config), nextConfig);
 };
 
